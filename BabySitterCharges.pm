44c326a0a2e61450e0a0ddf31de41569abb2eab7
Chelsea <gross.chelsea2010@gmail.com>
2016-02-09 17:27:28 -0500

    Add two tests for not saving time before 5:00 AM

--- BabySitterCharges.pm
+++ BabySitterCharges.pm
@@ -12,8 +12,26 @@ sub new {
 
 sub set_start_time($) {
     my ( $self, $start_time ) = @_;
-    $self->{start_time} = $start_time;
-    return 1;
+
+    my @tmp = split(':',$start_time);
+    my $hour = $tmp[0];
+
+    # Add 12 to the hour if it's in the PM
+    if ( $start_time ~~ /[PM]/ ) {
+        $hour += 12;
+    }
+
+    if ( $hour >= 17 ) {
+        $self->{start_time} = $start_time;
+        $self->{start_hour} = $hour;
+        return 1;
+    }
+    else {
+        # Don't save it if it's before 5 PM
+        $self->{start_time} = undef;
+        $self->{start_hour} = undef;
+        return 0;
+    }
 }
 
 sub get_start_time() {
--- charge_test.t
+++ charge_test.t
@@ -10,4 +10,7 @@ my $bsc = new BabySitterCharges();
 isa_ok($bsc, 'BabySitterCharges');
 
 ok($bsc->set_start_time("5:00 PM"), "Setting start time to 5:00 PM is successful");
-ok($bsc->get_start_time(), "Retrieve start time of 5:00 PM");
+is($bsc->get_start_time(), "5:00 PM", "Retrieve start time of 5:00 PM");
+
+ok(!$bsc->set_start_time("4:00 PM"), "Setting start time to 4:00 PM is un-successful");
+isnt($bsc->get_start_time(), "4:00 PM", "Verify start time didn't save");
