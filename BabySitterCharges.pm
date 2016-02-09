961d1856d835da12b0d1c4952d806e19f527d229
Chelsea <gross.chelsea2010@gmail.com>
2016-02-09 16:22:01 -0500

    Test passing for object creation

--- BabySitterCharges.pm
+++ BabySitterCharges.pm
@@ -3,4 +3,11 @@ package BabySitterCharges;
 use strict;
 use warnings;
 
+sub new {
+    my ( $class, %args ) = @_;
+
+    my $self = bless{}, $class;
+    return $self;
+};
+
 1;
--- charge_test.t
+++ charge_test.t
@@ -4,3 +4,7 @@ use warnings;
 use Test::More 'no_plan';
 
 use_ok 'BabySitterCharges';
+
+my $bsc = new BabySitterCharges();
+
+isa_ok($bsc, 'BabySitterCharges');
